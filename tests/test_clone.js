997f9c1e0b67bd56a8579d958644a8e630db930c
Marco Tabini <marcot@tabini.ca>
2013-07-11 09:38:01 -0400

    - Added serialization

--- tests/test_clone.js
+++ tests/test_clone.js
@@ -47,4 +47,50 @@ describe('The finite-state machine cloning system', function() {
         right.run(20);
     });
     
+    it('should allow serializing a fsm', function() {
+        var hasRun = false;
+       
+        var fsm = new Chine('initial');
+        
+        fsm.state(function() {
+            
+            this.name('initial');
+            
+            this.outgoing('step1');
+            
+            this.run(function() {
+                this.machine.test = 'test';
+                this.transition('step1');
+            });
+            
+        });
+        
+        fsm.state(function() {
+            
+            this.name('step1');
+            
+            this.incoming('initial');
+            
+            this.run(function(compare) {
+                expect(this.machine.test).to.equal('test');
+                hasRun = true;
+            });
+            
+        });
+        
+        fsm.compile();
+
+        fsm.run();
+        
+        var serialized = fsm.serialize();
+        
+        var unserialized = fsm.unserialize(serialized);
+        
+        expect(unserialized).not.to.equal(fsm);
+        
+        unserialized.run();
+        
+        expect(hasRun).to.equal(true);
+    });
+    
 });
